9e2f22b426fdf5a6f8007f339154ac63ea03e28f
gaeprivitello <gaeprivi>
2019-04-30 18:07:46 -0300

    Fix login github

--- config/services.php
+++ config/services.php
@@ -47,7 +47,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/login/github/callback'
+        'redirect' => 'http://iaw-app-2019.herokuapp.com/login/github/callback'
     ],
 
     'facebook' => [
